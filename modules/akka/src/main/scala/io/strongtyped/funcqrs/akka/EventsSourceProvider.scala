0e5b4d20cb10ce3fd81d75dd478d6f83801ff078
Renato Cavalcanti <renato@strongtyped.io>
2015-10-20 00:25:21 +0200

    don't need to be an actor

--- modules/akka/src/main/scala/io/strongtyped/funcqrs/akka/EventsSourceProvider.scala
+++ modules/akka/src/main/scala/io/strongtyped/funcqrs/akka/EventsSourceProvider.scala
@@ -1,6 +1,5 @@
 package io.strongtyped.funcqrs.akka
 
-import akka.actor.Actor
 import akka.persistence.query.EventEnvelope
 import akka.stream.scaladsl.Source
 
@@ -8,7 +7,7 @@ import akka.stream.scaladsl.Source
  * Provides an Akka-Streams [[Source]] that produces [[EventEnvelope]]s.
  */
 trait EventsSourceProvider {
-  this: Actor =>
 
   def source(offset: Long): Source[EventEnvelope, Unit]
+
 }
