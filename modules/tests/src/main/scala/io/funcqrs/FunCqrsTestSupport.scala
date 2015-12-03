a35e69566599be4512d16a821c43fc6b6972e86a
Renato Cavalcanti <renato@strongtyped.io>
2015-12-03 21:15:51 +0100

    WIP

--- modules/tests/src/main/scala/io/funcqrs/FunCqrsTestSupport.scala
+++ modules/tests/src/main/scala/io/funcqrs/FunCqrsTestSupport.scala
@@ -5,15 +5,101 @@ import scala.concurrent.{ ExecutionContext, Future }
 
 trait FunCqrsTestSupport {
 
-  def sendToProjection(projection: Projection, events: Seq[DomainEvent])(implicit ec: ExecutionContext): Future[Unit] = {
-    events.foldLeft(Future.successful(())) { (fut, evt) =>
-      fut.flatMap { _ => projection.onEvent(evt) }
+
+  class End2EndTestSupport(projection: Projection) extends WriteModelOps with ReadModelOps {
+
+    implicit class BehaviorOps[A <: AggregateLike](val behavior: Behavior[A]) {
+
+      def sendCreateCommand(cmd: behavior.Command)(implicit ec: ExecutionContext): Future[(behavior.Event, behavior.Aggregate)] = {
+        for {
+          (evt, aggregate) <- sendCreateCommandInternal(behavior)(cmd)
+          _ <- sendToProjectionInternal(projection, evt)
+        } yield (evt, aggregate)
+      }
+
+      def sendCommands(cmd: behavior.Command, cmds: behavior.Command*)(implicit ec: ExecutionContext): Future[(behavior.Events, behavior.Aggregate)] = {
+        for {
+          (evts, aggregate) <- sendCommandsInternal(behavior)(cmd, cmds: _*)
+          _ <- sendToProjectionInternal(projection, evts)
+        } yield (evts, aggregate)
+      }
+
+      def sendUpdateCommands(aggregate: behavior.Aggregate, cmds: behavior.Command*)(implicit ec: ExecutionContext): Future[(behavior.Events, behavior.Aggregate)] = {
+        for {
+          (evts, aggregate) <- sendUpdateCommandsInternal(behavior)(immutable.Seq(), aggregate, cmds: _*)
+          _ <- sendToProjectionInternal(projection, evts)
+        } yield (evts, aggregate)
+      }
+    }
+
+  }
+
+  trait ReadModelTestSupport extends ReadModelOps{
+
+    implicit class ProjectionOps(projection: Projection)(implicit ec: ExecutionContext) {
+
+      def sendToProjection(event: DomainEvent): Future[Unit] = {
+        sendToProjectionInternal(projection, event)
+      }
+
+      def sendToProjection(events: Seq[DomainEvent]): Future[Unit] = {
+        sendToProjectionInternal(projection, events)
+      }
+    }
+
+  }
+
+  trait WriteModelTestSupport extends WriteModelOps {
+
+    implicit class BehaviorOps[A <: AggregateLike](val behavior: Behavior[A]) {
+
+      def sendCreateCommand(cmd: behavior.Command)(implicit ec: ExecutionContext): Future[(behavior.Event, behavior.Aggregate)] = {
+        sendCreateCommandInternal(behavior)(cmd)
+      }
+
+      def sendCommands(cmd: behavior.Command, cmds: behavior.Command*)(implicit ec: ExecutionContext): Future[(behavior.Events, behavior.Aggregate)] = {
+        sendCommandsInternal(behavior)(cmd, cmds: _*)
+      }
+
+      def sendUpdateCommands(aggregate: behavior.Aggregate, cmds: behavior.Command*)(implicit ec: ExecutionContext): Future[(behavior.Events, behavior.Aggregate)] = {
+        sendUpdateCommandsInternal(behavior)(immutable.Seq(), aggregate, cmds: _*)
+      }
     }
   }
 
-  def sendCommands[A <: AggregateLike](behavior: Behavior[A])(cmd: behavior.Command, cmds: behavior.Command*)(implicit ec: ExecutionContext): Future[(behavior.Events, behavior.Aggregate)] = {
+  trait ReadModelOps {
 
-    def applyCommands(events: behavior.Events, aggregate: behavior.Aggregate, cmds: behavior.Command*): Future[(behavior.Events, behavior.Aggregate)] = {
+    protected def sendToProjectionInternal(projection: Projection, event: DomainEvent)(implicit ec: ExecutionContext): Future[Unit] = {
+      projection.onEvent(event)
+    }
+
+    protected def sendToProjectionInternal(projection: Projection, events: Seq[DomainEvent])(implicit ec: ExecutionContext): Future[Unit] = {
+      events.foldLeft(Future.successful(())) { (fut, evt) =>
+        fut.flatMap { _ => projection.onEvent(evt) }
+      }
+    }
+  }
+
+  trait WriteModelOps {
+
+    protected def sendCreateCommandInternal[A <: AggregateLike](behavior: Behavior[A])
+                                                               (cmd: behavior.Command, cmds: behavior.Command*)
+                                                               (implicit ec: ExecutionContext): Future[(behavior.Event, behavior.Aggregate)] = {
+      behavior.applyCommand(cmd)
+    }
+    protected def sendCommandsInternal[A <: AggregateLike](behavior: Behavior[A])
+                                                          (cmd: behavior.Command, cmds: behavior.Command*)
+                                                          (implicit ec: ExecutionContext): Future[(behavior.Events, behavior.Aggregate)] = {
+
+      sendCreateCommandInternal(behavior)(cmd).flatMap {
+        case (evt, agg) => sendUpdateCommandsInternal(behavior)(immutable.Seq(evt), agg, cmds: _*)
+      }
+    }
+
+    protected def sendUpdateCommandsInternal[A <: AggregateLike](behavior: Behavior[A])
+                                                                (events: behavior.Events, aggregate: behavior.Aggregate, cmds: behavior.Command*)
+                                                                (implicit ec: ExecutionContext): Future[(behavior.Events, behavior.Aggregate)] = {
+
       cmds.toList match {
         case head :: Nil => behavior.applyCommand(head, aggregate).map {
           case (evts, agg) =>
@@ -25,50 +111,12 @@ trait FunCqrsTestSupport {
           case (evts, agg) =>
             // concat previous events with events from last command
             val allEvents = events ++ evts
-            applyCommands(allEvents, agg, tail: _*)
+            sendUpdateCommandsInternal(behavior)(allEvents, agg, tail: _*)
         }
-        case Nil => Future.failed(new RuntimeException("Not commands found!!"))
-      }
-    }
-
-    behavior.applyCommand(cmd).flatMap {
-      case (evt, agg) =>
-        applyCommands(immutable.Seq(evt), agg, cmds: _*)
-    }
-  }
-
-  class End2EndTestSupport(projection: Projection) {
-
-    implicit class BehaviorOps[A <: AggregateLike](behavior: Behavior[A]) {
-
-      def applyCommands(cmd: behavior.Command, cmds: behavior.Command*)(implicit ec: ExecutionContext): Future[Unit] = {
-        val resultWriteModel = sendCommands(behavior)(cmd, cmds: _*)
-        resultWriteModel.flatMap {
-          case (evts, _) => sendToProjection(projection, evts)
-        }
-      }
-    }
-
-  }
-
-  trait ReadModelTestSupport {
-
-    implicit class ProjectionOps(projection: Projection)(implicit ec: ExecutionContext) {
-
-      def applyEvents(events: Seq[DomainEvent]): Future[Unit] = {
-        sendToProjection(projection, events)
-      }
-    }
-
-  }
-
-  trait WriteModelTestSupport {
-
-    implicit class BehaviorOps[A <: AggregateLike](val behavior: Behavior[A]) {
-      def applyCommands(cmd: behavior.Command, cmds: behavior.Command*)(implicit ec: ExecutionContext): Future[(behavior.Events, behavior.Aggregate)] = {
-        sendCommands(behavior)(cmd, cmds: _*)
+        case Nil => Future.successful((events, aggregate))
       }
     }
   }
+
 }
 
