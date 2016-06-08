ffe7988474284b536d186b5403d524e3efc5e779
Renato Cavalcanti <renato@strongtyped.io>
2016-06-08 17:51:11 +0200

    release 0.4.6

--- modules/tests/src/test/scala/io/funcqrs/model/TimeTracker.scala
+++ modules/tests/src/test/scala/io/funcqrs/model/TimeTracker.scala
@@ -5,27 +5,76 @@ import java.util.UUID
 
 import io.funcqrs._
 import io.funcqrs.behavior._
-import io.funcqrs.behavior.{ Behavior, Uninitialized }
+import io.funcqrs.model.TimerTrackerProtocol.{ TimerStarted, TimerStopped }
 
-case class TimeTracker(id: TrackerId, currentTask: Option[Task] = None, previousTasks: List[Task] = List()) extends AggregateLike {
+sealed trait TimeTracker extends AggregateLike {
+
   type Id = TrackerId
   type Protocol = TimerTrackerProtocol.type
 
-  def isBusy = currentTask.isDefined
-  def isIdle = currentTask.isEmpty
+  def id: TrackerId
+  def previousTasks: List[Task]
 
-  def start(title: String, start: OffsetDateTime) = {
-    val task = Task(title, start)
-    copy(currentTask = Some(task))
-  }
+  def isBusy =
+    this match {
+      case _: BusyTracker => true
+      case _ => false
+    }
 
-  def stop(end: OffsetDateTime) = {
-    currentTask.map { task =>
-      val stoppedTask = task.copy(stopped = Some(end))
-      copy(currentTask = None, previousTasks = previousTasks :+ task)
-    }.getOrElse(this)
-  }
+  def isIdle = !isBusy
 }
+
+case class BusyTracker(id: TrackerId, currentTask: Task, previousTasks: List[Task] = List()) extends TimeTracker {
+
+  import TimerTrackerProtocol._
+
+  def stop(end: OffsetDateTime): TimeTracker = {
+    IdleTracker(id, previousTasks :+ currentTask)
+  }
+
+  def actionsWhenBusy =
+    actions[TimeTracker]
+      .handleCommand {
+        cmd: StopTracking.type => TimerStopped(OffsetDateTime.now(), EventId(), cmd.id)
+      }
+      .handleCommand.manyEvents {
+        cmd: ReplaceTask =>
+          val now = OffsetDateTime.now()
+          List(
+            TimerStopped(now, EventId(), cmd.id),
+            // the event handler for TimerStarter is defined
+            // on the behavior case branch for idling TimeTracker
+            TimerStarted(cmd.title, now, EventId(), cmd.id)
+          )
+      }
+      .handleEvent {
+        evt: TimerStopped => stop(evt.end)
+      }
+}
+
+case class IdleTracker(id: TrackerId, previousTasks: List[Task] = List()) extends TimeTracker {
+
+  import TimerTrackerProtocol._
+
+  def start(title: String, start: OffsetDateTime): TimeTracker = {
+    val task = Task(title, start)
+    BusyTracker(id, task, previousTasks)
+  }
+
+  def actionsWhenIdle =
+    actions[TimeTracker]
+      .handleCommand {
+        cmd: StartTracking => TimerStarted(cmd.title, OffsetDateTime.now(), EventId(), cmd.id)
+      }
+      .handleCommand {
+        cmd: ReplaceTask => TimerStarted(cmd.title, OffsetDateTime.now(), EventId(), cmd.id)
+      }
+      .handleEvent {
+        evt: TimerStarted => start(evt.title, evt.start)
+      }
+
+}
+
 case class Task(title: String, started: OffsetDateTime, stopped: Option[OffsetDateTime] = None) {
   def isRunning = stopped.isEmpty
 }
@@ -47,76 +96,32 @@ object TimeTracker {
 
   import TimerTrackerProtocol._
 
-  def behavior(id: TrackerId): Behavior[TimeTracker] = {
+  def factoryActions(id: TrackerId) =
+    actions[TimeTracker]
+      .handleCommand {
+        cmd: CreateTracker.type => TimerCreated(EventId(), cmd.id)
+      }
+      .handleCommand.manyEvents {
+        cmd: CreateAndStartTracking =>
+          List(
+            TimerCreated(EventId(), cmd.id),
+            // the event handler for TimerStarter depends on a created Tracker
+            // and therefore can't be defined in this 'Actions'
+            // behavior is available in next transition
+            TimerStarted(cmd.taskTitle, OffsetDateTime.now(), EventId(), cmd.id)
+          )
+      }
+      .handleEvent {
+        evt: TimerCreated => IdleTracker(id)
+      }
 
-    case Uninitialized(_) =>
-      actions[TimeTracker]
-        .handleCommand {
-          cmd: CreateTracker.type => TimerCreated(EventId(), cmd.id)
-        }
-        .handleCommand.manyEvents {
-          cmd: CreateAndStartTracking =>
-            List(
-              TimerCreated(EventId(), cmd.id),
-              // the event handler for TimerStarter depends on a created Tracker
-              // and therefore can't be defined in this 'Actions'
-              // behavior is available in next transition
-              TimerStarted(cmd.taskTitle, OffsetDateTime.now(), EventId(), cmd.id)
-            )
-        }
-        .handleEvent {
-          evt: TimerCreated => TimeTracker(id)
-        }
-
-    case Idle(tracker) =>
-      actions[TimeTracker]
-        .handleCommand {
-          cmd: StartTracking => TimerStarted(cmd.title, OffsetDateTime.now(), EventId(), cmd.id)
-        }
-        .handleCommand {
-          cmd: ReplaceTask => TimerStarted(cmd.title, OffsetDateTime.now(), EventId(), cmd.id)
-        }
-        .handleEvent {
-          evt: TimerStarted => tracker.start(evt.title, evt.start)
-        }
-
-    case Busy(tracker) =>
-      actions[TimeTracker]
-        .handleCommand {
-          cmd: StopTracking.type => TimerStopped(OffsetDateTime.now(), EventId(), cmd.id)
-        }
-        .handleCommand.manyEvents {
-          cmd: ReplaceTask =>
-            val now = OffsetDateTime.now()
-            List(
-              TimerStopped(now, EventId(), cmd.id),
-              // the event handler for TimerStarter is defined
-              // on the behavior case branch for idling TimeTracker
-              TimerStarted(cmd.title, now, EventId(), cmd.id)
-            )
-        }
-        .handleEvent {
-          evt: TimerStopped => tracker.stop(evt.end)
-        }
-  }
-
-  class Busy(val state: Initialized[TimeTracker]) extends AnyVal {
-    def isEmpty = state.aggregate.isIdle
-    def get = state.aggregate
-  }
-
-  object Busy {
-    def unapply(state: Initialized[TimeTracker]): Busy = new Busy(state)
-  }
-
-  class Idle(val obj: Initialized[TimeTracker]) extends AnyVal {
-    def isEmpty = obj.aggregate.isBusy
-    def get = obj.aggregate
-  }
-
-  object Idle {
-    def unapply(obj: Initialized[TimeTracker]): Idle = new Idle(obj)
-  }
+  def behavior(id: TrackerId): Behavior[TimeTracker] =
+    Behavior {
+      factoryActions(id)
+    } {
+      case tracker: IdleTracker => tracker.actionsWhenIdle
+      case tracker: BusyTracker => tracker.actionsWhenBusy
+    }
 
 }
 
